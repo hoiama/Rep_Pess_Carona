935ff5fdbc02abb2a8104d07c4add18853823d64
hoiama <hoiama.costa@cwi.com.br>
2018-07-31 09:57:32 -0300

    atualizando com spring

--- angular1/src/main/java/br/com/hoiama/angular1/main/Angular1Application.java
+++ angular1/src/main/java/br/com/hoiama/angular1/main/Angular1Application.java
@@ -1,9 +1,12 @@
-package br.com.hoiama.angular1;
+package br.com.hoiama.angular1.main;
 
+import br.com.hoiama.angular1.controller.ControllerIndex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = ControllerIndex.class)
 public class Angular1Application {
 
 	public static void main(String[] args) {
