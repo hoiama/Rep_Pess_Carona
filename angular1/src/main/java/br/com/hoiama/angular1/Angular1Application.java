5e4094d1962e946f1f7a4b390b2461b00c829adc
hoiama <hoiama.costa@cwi.com.br>
2018-07-31 09:05:48 -0300

    integrando spring

---
+++ angular1/src/main/java/br/com/hoiama/angular1/Angular1Application.java
@@ -0,0 +1,12 @@
+package br.com.hoiama.angular1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular1Application.class, args);
+	}
+}
---
+++ angular1/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hoiama
+  Date: 7/31/18
+  Time: 8:50 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
